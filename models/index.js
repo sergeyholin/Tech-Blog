c0c216a7d4dce5799420b7cae77b98ca63d3e698
Sergey Holin <holin_sergey@yahoo.com>
2022-08-17 22:08:03 -0700

    app is fully functional

--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
-// User.hasMany(Blog, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE',
-// });
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
@@ -17,15 +17,15 @@ Comment.belongsTo(User, {
   onDelete: 'CASCADE',
 });
 
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE',
-// })
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+})
 
-// Comment.belongsTo(Blog, {
-//   foreignKey: 'blog_id',
-//   onDelete: 'CASCADE',
-// })
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE',
+})
 
 Blog.hasMany(Comment, {
   foreignKey: 'blog_id',
